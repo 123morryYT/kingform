8bc1a440b4c17b714c258928be38d873315c2ec2
123morryYT <44780155+123morryYT@users.noreply.github.com>
2019-05-07 11:20:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,52 +1,88 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const prefix = '+'
-
+const client2 = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`+help | Dark server`,"http://twitch.tv/S-F")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
+   console.log(`----------------`);
+   console.log(`Credit Farmm - Script By : Kahrbaa `);
+   console.log(`----------------`);
+   console.log(`Loadinng`);
+   console.log(`Loadinng.`);
+   console.log(`Loadinng..`);
+   console.log(`Loadinng...`);
+   console.log(`This Bots Online ' `);
+   console.log(`----------------`);
+});
+
+
+client.on('message', message => {
+    if(message.content === '-راتب'){
+        message.channel.send('#daily')
+    }
+});
+
+client.on('message', message => {
+    if(message.content === '-مبلغ'){
+        message.channel.send('#credits')
+    }
+});
+
+client.on('message', message => {
+    if(message.content === '-ريب'){
+        message.channel.send('#credits')
+    }
+});
+
+client2.on('message', message => {
+    if(message.content === '-راتب'){
+        message.channel.send('#daily')
+    }
+});
+
+client2.on('message', message => {
+    if(message.content === '-مبلغ'){
+        message.channel.send('#credits')
+    }
+});
+
+client2.on('message', message => {
+    if(message.content === '-ريب'){
+        message.channel.send('#credits')
+    }
+});
+
+
+
+client.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client2.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
 });
 
 
 
 
-
-
-
-
-
-client.on('ready', async() => {
-var server = "505417459369508884"; // ايدي السررفر
-var channel = "508038901789884416";//ايدي الروم
-    setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**dark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3fdl serverdark server 3f**')
-    },305);
-})
-
-
-
-
-
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.TOKEN);// لا تغير فيها شيء
+client2.login(process.env.TOKEN2);// لا تغير فيها شيء
